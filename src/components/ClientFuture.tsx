cc928bd938e29f0032a68cc2e5a3221212c015b5
Mickael Joseph <mickaelandriana06@gmail.com>
2025-05-23 02:23:42 +0300

    bugs

--- src/components/ClientFuture.tsx
+++ src/components/ClientFuture.tsx
@@ -28,6 +28,9 @@ const header = [
 ];
 const ClientFuture = () => {
   const [data, setData] = useState<ClientData[]>([initData]);
+  const [add, setAdd] = useState(false);
+  const [update, setUpdate] = useState(false);
+
   return (
     <>
       <div className="w-full flex justify-between items-center  pl-3 mt-20 mb-10">
@@ -37,6 +40,9 @@ const ClientFuture = () => {
           </h3>
           <p className="text-slate-500">Apperçus des clients .</p>
         </div>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full ml-100" onClick={()=> setAdd(true)}>
+          Ajout
+        </button>
         <div className="ml-3">
           <div className="w-full max-w-sm min-w-[200px] relative">
             <div className="relative">
@@ -144,6 +150,64 @@ const ClientFuture = () => {
           </tbody>
         </table>
       </div>
+      {add && (
+        <form className="w-full max-w-sm">
+          <div className="md:flex md:items-center mb-6">
+            <div className="md:w-1/3">
+              <label
+                className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                htmlFor="inline-full-name"
+              >
+                Full Name
+              </label>
+            </div>
+            <div className="md:w-2/3">
+              <input
+                className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                id="inline-full-name"
+                type="text"
+                value="Jane Doe"
+              />
+            </div>
+          </div>
+          <div className="md:flex md:items-center mb-6">
+            <div className="md:w-1/3">
+              <label
+                className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                htmlFor="inline-password"
+              >
+                Password
+              </label>
+            </div>
+            <div className="md:w-2/3">
+              <input
+                className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                id="inline-password"
+                type="password"
+                placeholder="******************"
+              />
+            </div>
+          </div>
+          <div className="md:flex md:items-center mb-6">
+            <div className="md:w-1/3"></div>
+            <label className="md:w-2/3 block text-gray-500 font-bold">
+              <input className="mr-2 leading-tight" type="checkbox" />
+              <span className="text-sm">Send me your newsletter!</span>
+            </label>
+          </div>
+          <div className="md:flex md:items-center">
+            <div className="md:w-1/3"></div>
+            <div className="md:w-2/3">
+              <button
+                className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                type="button"
+              >
+                Sign Up
+              </button>
+            </div>
+          </div>
+        </form>
+      )}
     </>
   );
 };
