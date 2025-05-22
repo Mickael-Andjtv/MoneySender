d67617273d282d4c4df86734824d444e19ae40b9
Mickael Joseph <mickaelandriana06@gmail.com>
2025-05-23 01:45:18 +0300

    change style

--- src/components/Pages/Client.tsx
+++ src/components/Pages/Client.tsx
@@ -3,7 +3,7 @@ import Navbar from "../Navbar";
 
 export default function Client() {
   return (
-    <div>
+    <div className="flex flex-col">
       <Navbar />
       <ClientFuture />
     </div>
