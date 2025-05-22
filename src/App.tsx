9dacd7a0e6ab512309407c48c59da07ac75ae9f4
Mickael Joseph <mickaelandriana06@gmail.com>
2025-05-23 01:05:29 +0300

    create client component dans its table

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import "./App.css";
+import Client from "./components/Client";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div >
+    <div>
       <Navbar />
+      <Client />
     </div>
   );
 }
--- src/components/Client.tsx
+++ src/components/Client.tsx
@@ -1,5 +1,50 @@
+import { useState } from "react";
+
+interface ClientData {
+  numTel: string;
+  nom: string;
+  sexe: string;
+  pays: string;
+  solde: number;
+  mail: string;
+}
+
+const initData = {
+  numTel: "",
+  nom: "",
+  sexe: "",
+  pays: "",
+  solde: 0,
+  mail: "",
+};
+const header = ["Numéro Téléphone", "Nom", "Sexe", "Pays", "Solde", "Mail"];
 const Client = () => {
-  return <div></div>;
+  const [data, setData] = useState<ClientData[]>([initData]);
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            {header.map((h, i) => (
+              <th key={i}> h </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item) => (
+            <tr key={item.numTel}>
+              <td> {item.numTel} </td>
+              <td> {item.nom} </td>
+              <td> {item.sexe} </td>
+              <td> {item.pays} </td>
+              <td> {item.solde} </td>
+              <td> {item.mail} </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
 };
 
 export default Client;
