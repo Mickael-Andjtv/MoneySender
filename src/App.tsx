97c511d7fbd2ac628b457ad7ddaebdba841b55f2
Mickael Joseph <mickaelandriana06@gmail.com>
2025-05-23 00:37:12 +0300

    fix bug of style navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
-import Navbar from './components/Navbar'
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-
   return (
-    <>
-      <Navbar/>
-    </>
-  )
+    <div >
+      <Navbar />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ export default function Navbar() {
   const links = ["Envoyer", "Client", "Taux", "Frais"];
   return (
     <>
-      <div className="flex flex-row font-mono text-base">
+      <div className="flex flex-row font-mono text-base ">
         {links.map((link, index) => (
           <a href="#" className="basis-64" key={index}>
             {" "}
