b67d30ab5443f2ba67d81f0f92940a45aa903114
Mickael Joseph <mickaelandriana06@gmail.com>
2025-05-24 22:15:18 +0300

    udpate app component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import "./App.css";
+import Navbar from "./components/Navbar";
 import Client from "./components/Pages/Client";
 
 function App() {
   return (
     <div className="font-mono">
+      <Navbar />
       <Client />
     </div>
   );
