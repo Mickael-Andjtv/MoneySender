61c10f209933c3553453112c9cc5ea6db4875d58
Mickael Joseph <mickaelandriana06@gmail.com>
2025-05-24 20:04:18 +0300

    create interface dataTaux

--- src/components/services/Data.ts
+++ src/components/services/Data.ts
@@ -1,3 +1,14 @@
-export interface DataClient{
-    
+export interface DataClient {
+  numTel: string;
+  nom: string;
+  sexe: string;
+  pays: string;
+  solde: number;
+  mail: string;
+}
+
+export interface DataTaux {
+  idTaux: string;
+  montant1: number;
+  montant2: number;
 }
