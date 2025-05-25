d66c85ab1b965abe8a059bf9030c649ac4471a79
Mickael Joseph <mickaelandriana06@gmail.com>
2025-05-25 15:02:52 +0300

    create props for navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+import { useState } from "react";
 import "./App.css";
 import ErrorBoundary from "./components/Errors/ErrorBoundary";
 import Navbar from "./components/Navbar";
+import Client from "./components/Pages/Client";
 import Frais from "./components/Pages/Frais";
-// import Client from "./components/Pages/Client";
-// import Taux from "./components/Pages/Taux";
+import Taux from "./components/Pages/Taux";
 
+const pages = ["envoyer", <Client/>,<Taux/>, <Frais/>]
 function App() {
+  const [page, setPage] = useState(0)
   return (
     <div className="font-mono">
       <Navbar />
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,18 @@
-export default function Navbar() {
+interface NavbarProps {
+  handlepage: (page: number) => void;
+}
+export default function Navbar({ handlepage }: NavbarProps) {
   const links = ["Envoyer", "Client", "Taux", "Frais"];
   return (
     <>
       <div className="flex flex-row  text-base ">
         {links.map((link, index) => (
-          <a href="#" className="basis-64 hover:bg-blue-100 p-2 rounded-lg" key={index}>
+          <a
+            href="#"
+            className="basis-64 hover:bg-blue-100 p-2 rounded-lg"
+            key={index}
+            onClick={() => handlepage(index)}
+          >
             {" "}
             {link}{" "}
           </a>
