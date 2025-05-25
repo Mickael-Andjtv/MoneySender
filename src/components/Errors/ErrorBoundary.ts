5301e4d52a4458b5ed672c58ee0cda78bff1c80f
Mickael Joseph <mickaelandriana06@gmail.com>
2025-05-25 08:28:07 +0300

    fix import to a type

--- src/components/Errors/ErrorBoundary.ts
+++ src/components/Errors/ErrorBoundary.ts
@@ -1,4 +1,5 @@
-import React, { Component, ErrorInfo, ReactNode } from "react";
+import  { Component, type ReactNode } from "react"; 
+import type { ErrorInfo } from "react"; 
 
 interface Props {
   children: ReactNode;
