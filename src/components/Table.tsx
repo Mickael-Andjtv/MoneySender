6d1dcf0ccfd5999b7ab3cf254e6a643ce9d23754
Mickael Joseph <mickaelandriana06@gmail.com>
2025-05-27 07:57:21 +0300

    add some logic for search by date in table

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -39,9 +39,10 @@ export default function Table<
             <div className="w-full max-w-sm min-w-[200px] relative">
               <div className="relative">
                 <input 
+                type={'numTel' in data? "text":"date"}
                   onChange={(e)=> handeSearch && handeSearch(e.target.value)}
                   className="bg-white w-full pr-11 h-10 pl-3 py-2 bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md"
-                  placeholder="Search for invoice..."
+                  placeholder={`rechercher...`}
                 />
                 <button
                   className="absolute h-8 w-8 right-1 top-1 my-auto px-2 flex items-center bg-white rounded "
